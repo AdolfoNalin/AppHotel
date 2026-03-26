905aed3485609289f13306721be3a348190cf666
AdolfoNalin <nalinadolfo33@gmail.com>
2026-03-26 20:10:39 -0300

    Start the app Hotel

---
+++ App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using AppHotel.View;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new Menu();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Height = 700;
+            window.Width = 400;
+
+            return window;
+        }
+    }
+}

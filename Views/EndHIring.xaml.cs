9e666407cb3cb292a8080049ccdc18daea00a2a3
AdolfoNalin <nalinadolfo33@gmail.com>
2026-03-26 21:30:52 -0300

    Rename the folder View for Views

---
+++ Views/EndHIring.xaml.cs
@@ -0,0 +1,9 @@
+namespace AppHotel.Views;
+
+public partial class EndHIring : ContentPage
+{
+	public EndHIring()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Views/HiringMenu.xaml.cs
@@ -0,0 +1,9 @@
+namespace AppHotel.Views;
+
+public partial class HiringMenu : ContentPage
+{
+	public HiringMenu()
+	{
+		InitializeComponent();
+	}
+}
